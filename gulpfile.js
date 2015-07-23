6d291836eb9aa05a60e3aa1bfc52f49099dec14d
Man YUAN <epsilony@cocon.com.cn>
2015-07-23 17:08:37 +0800

    init commit

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+'use strict';
+var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+var shelljs = require('shelljs');
+
+function buildBundle(){
+  shelljs.exec('jspm bundle ./lib/main dist/flatgauge.js --inject');
+}
+
+gulp.task('bundle', function() {
+  buildBundle();
+});
+
+gulp.task('watch', ['bundle'], function() {
+
+  browserSync.init({
+    server: "."
+  });
+
+  function watchBundle() {
+    buildBundle();
+    browserSync.reload();
+  }
+  gulp.watch("lib/**/*.{js,html,css}").on('change', watchBundle);
+  gulp.watch("*.html",browserSync.reload());
+});
+
+gulp.task('default', ['watch']);
